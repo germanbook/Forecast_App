cce7111e22887b9ce774ecff73c829b5dd525063
BOWEN ZHANG <germanbook@hotmail.com>
2023-02-21 22:22:17 +1300

    Initial commit

---
+++ app/src/main/java/com/example/forecastapp/ui/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.forecastapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toolbar
+import androidx.navigation.NavAction
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.forecastapp.R
+import com.example.forecastapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var bottomNav: BottomNavigationView
+    private lateinit var navHostFragment : NavHostFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+}
